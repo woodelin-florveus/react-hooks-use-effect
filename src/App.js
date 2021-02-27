6618ee63ec62c38f88d1e38cb7fcbaec4a36d4e5
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-27 10:17:49 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics"
 
 function App() {
-  useEffect(() => {
-    console.log("useEffect called");
-  });
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
 
-  console.log("Component rendering");
+  useEffect(
+    // 1st arg: side effect (callback function)
+    () => console.log("useEffect called"),
+    // 2nd arg: dependencies array
+    [count]
+  );
+  // console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+{/* <DogPics /> */}
+
+
+    </div>
+
+  );
 }
+
 
 export default App;
